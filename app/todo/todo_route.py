02fcd013f65839bafd6bd8ddb4b4b596ea67afef
bomsiwor <bomsiwor@gmail.com>
2024-12-15 11:00:27 +0700

    feat/todo : get single todo by id

--- app/todo/todo_route.py
+++ app/todo/todo_route.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 from typing import List, Optional
 from pydantic import BaseModel
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 
-from core.helper.responses import ResponseModel, SuccessResponse
+from core.helper.responses import ErrorResponse, ResponseModel, SuccessResponse
 
 
 class Todo(BaseModel):
@@ -31,3 +31,19 @@ def get_all():
     result = [todo for todo in todos if not todo.deleted_at]
 
     return SuccessResponse(data=result)
+
+
+@todo_router.get(
+    "/{todo_id}",
+    status_code=200,
+    summary="Get Todo by ID",
+    description="Get single Todo by given ID",
+    response_model=ResponseModel[Todo | None],
+)
+def get_single_by_id(todo_id: str, response: Response):
+    for todo in todos:
+        if todo.id == todo_id:
+            return SuccessResponse[Todo](data=todo)
+
+    response.status_code = 404
+    return ErrorResponse[None](message="Todo Not found", code=404, data=None)
